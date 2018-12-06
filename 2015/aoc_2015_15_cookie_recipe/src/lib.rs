2652dc78da7d50a7b841db09809943ad82a450ec
Chris Houhoulis <chris@chrishouhoulis.com>
2018-12-06 11:00:01 -0500

    Recipe & Ingredient structs.

--- 2015/aoc_2015_15_cookie_recipe/src/lib.rs
+++ 2015/aoc_2015_15_cookie_recipe/src/lib.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs;
 
 #[derive(Debug)]
@@ -7,42 +6,51 @@ struct Ingredient {
     capacity: i64, durability: i64, flavor: i64, texture: i64, calories: i64
 }
 
-impl Ingredient {
-    fn multiply(&self, i: i64) -> Ingredient {
-        Ingredient {
-            name: format!("{} ({})", self.name, i),
-            capacity: self.capacity * i,
-            durability: self.durability * i,
-            flavor: self.flavor * i,
-            texture: self.texture * i,
-            calories: self.calories * i
-        }
+#[derive(Debug)]
+struct Recipe {
+    ingredients: Vec<Ingredient>
+}
+
+impl Recipe {
+    fn len(&self) -> usize {
+        self.ingredients.len()
+    }
+
+    fn score_with_multipliers(&self, multipliers: Vec<i64>) -> i64 {
+        if self.len() != multipliers.len() {
+            panic!("Ouchie!");
+        };
+        let ing_with_multipliers: Vec<(&Ingredient, i64)> = self.ingredients.iter().zip(multipliers).collect();
+        let cap: i64 = ing_with_multipliers.iter().map(|(ing, mult)| mult * ing.capacity).sum();
+        let dur: i64 = ing_with_multipliers.iter().map(|(ing, mult)| mult * ing.durability).sum();
+        let fla: i64 = ing_with_multipliers.iter().map(|(ing, mult)| mult * ing.flavor).sum();
+        let tex: i64 = ing_with_multipliers.iter().map(|(ing, mult)| mult * ing.texture).sum();
+        if cap < 0 || dur < 0 || fla < 0 || tex < 0 {
+            return 0;
+        };
+        cap * dur * fla * tex
     }
 }
 
 pub fn make_cookies(filename: String) -> i64 {
     let contents = read_file(filename);
-    let ings = parse_ingredients(contents);
-    let result: i64 = find_high_score(ings);
+    let recipe = parse_ingredients(contents);
+    let result: i64 = find_high_score(recipe);
     println!("Found score! {}", result);
     result
 }
 
 fn read_file(filename: String) -> String {
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    contents
+    fs::read_to_string(filename).expect("Something went wrong reading the file")
 }
 
-fn parse_ingredients(contents: String) -> Vec<Ingredient> {
+fn parse_ingredients(contents: String) -> Recipe {
     let lines = contents.lines();
-    let ings: Vec<Ingredient> = lines
+    let ingredients: Vec<Ingredient> = lines
         .map(|line| parse_ingredient(line.to_string()))
         .collect();
-    println!("hi {:?}", ings);
-    ings
+    println!("Ingredients that are going into recipe: {:?}", ingredients);
+    Recipe { ingredients }
 }
 
 fn parse_ingredient(text: String) -> Ingredient {
@@ -60,56 +68,31 @@ fn parse_ingredient(text: String) -> Ingredient {
     }
 }
 
-fn find_high_score(ingredients: Vec<Ingredient>) -> i64 {
-    let result = if ingredients.len() == 2 {
-        find_high_score_2(ingredients)
+fn find_high_score(recipe: Recipe) -> i64 {
+    if recipe.len() == 2 {
+        find_high_score_2(recipe)
     } else {
-        find_high_score_4(ingredients)
-    };
-    result
+        find_high_score_4(recipe)
+    }
 }
 
-fn score(ingredients: Vec<Ingredient>) -> i64 {
-    let cap: i64 = ingredients.iter().map(|ing| ing.capacity).sum();
-    let dur: i64 = ingredients.iter().map(|ing| ing.durability).sum();
-    let fla: i64 = ingredients.iter().map(|ing| ing.flavor).sum();
-    let tex: i64 = ingredients.iter().map(|ing| ing.texture).sum();
-    if cap < 0 || dur < 0 || fla < 0 || tex < 0 {
-        return 0;
-    };
-    cap * dur * fla * tex
-}
-
-fn find_high_score_2(ingredients: Vec<Ingredient>) -> i64 {
+fn find_high_score_2(recipe: Recipe) -> i64 {
     (0..100).map(|i|
-        score(vec![
-            ingredients[0].multiply(i),
-            ingredients[1].multiply(100 - i)
-        ])
-    ).max().expect("whatta")
+        recipe.score_with_multipliers(vec![i, 100 - i])
+    ).max().expect("No max score?!")
 }
 
-fn find_high_score_4(ingredients: Vec<Ingredient>) -> i64 {
+fn find_high_score_4(recipe: Recipe) -> i64 {
     let mut scores: Vec<i64> = Vec::new();
     for i in 0..=100 {
-        for j in 0..=(100-i) {
-            for k in 0..=(100-i-j) {
+        for j in 0..=(100 - i) {
+            for k in 0..=(100 - i - j) {
                 let l = 100 - i - j - k;
-                scores.push(score(vec![
-                    ingredients[0].multiply(i),
-                    ingredients[1].multiply(j),
-                    ingredients[2].multiply(k),
-                    ingredients[3].multiply(l),
-                ]));
+                scores.push(recipe.score_with_multipliers(vec![i, j, k, l]))
             }
         }
     };
-    println!("Better be better than 25s all around ({})", score(vec![
-        ingredients[0].multiply(25),
-        ingredients[1].multiply(25),
-        ingredients[2].multiply(25),
-        ingredients[3].multiply(25),
-    ]));
+    println!("Better be better than 25s all around! ({})", recipe.score_with_multipliers(vec![25, 25, 25, 25]));
 
-    scores.into_iter().max().expect("wowzers")
+    scores.into_iter().max().expect("Wowzers! No/bad scores for max()??!")
 }
