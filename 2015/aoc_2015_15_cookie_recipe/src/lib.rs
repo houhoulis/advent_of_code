437f5ece79d8368e1975c0d82e6665d0d9befe64
Chris Houhoulis <chris@chrishouhoulis.com>
2018-12-09 22:16:47 -0500

    High-score recipe, optionally for a calorie count.

--- 2015/aoc_2015_15_cookie_recipe/src/lib.rs
+++ 2015/aoc_2015_15_cookie_recipe/src/lib.rs
@@ -16,32 +16,46 @@ impl Recipe {
         self.ingredients.len()
     }
 
-    fn score_with_multipliers(&self, multipliers: &Vec<i64>) -> i64 {
+    fn score_with_multipliers(&self, multipliers: &Vec<i64>, calorie_limit: Option<i64>) -> Option<i64> {
         if self.len() != multipliers.len() {
-            panic!("Ouchie!");
+            panic!("Proportion count != ingredient count!");
         };
         let ing_with_multipliers: Vec<(&Ingredient, &i64)> = self.ingredients.iter().zip(multipliers).collect();
+        if let Some(limit) = calorie_limit {
+            let cal: i64 = ing_with_multipliers.iter().map(|(ing, &mult)| mult * ing.calories).sum();
+            if cal != limit {
+                return None;
+            }
+        };
         let cap: i64 = ing_with_multipliers.iter().map(|(ing, &mult)| mult * ing.capacity).sum();
         let dur: i64 = ing_with_multipliers.iter().map(|(ing, &mult)| mult * ing.durability).sum();
         let fla: i64 = ing_with_multipliers.iter().map(|(ing, &mult)| mult * ing.flavor).sum();
         let tex: i64 = ing_with_multipliers.iter().map(|(ing, &mult)| mult * ing.texture).sum();
         if cap < 0 || dur < 0 || fla < 0 || tex < 0 {
-            return 0;
+            return Some(0);
         };
-        cap * dur * fla * tex
+        Some(cap * dur * fla * tex)
     }
 }
 
-pub fn make_cookies(filename: String) -> i64 {
+pub fn make_cookies(filename: String) -> Option<i64> {
     let contents = read_file(filename);
     let recipe = parse_ingredients(contents);
-    let result: i64 = find_high_score(recipe);
-    println!("Found score! {}", result);
+    let result: Option<i64> = find_high_score(recipe, None);
+    println!("Found score! {:?}", result);
+    result
+}
+
+pub fn cookies_with_calories(filename: String, calorie_limit: i64) -> Option<i64> {
+    let contents = read_file(filename);
+    let recipe = parse_ingredients(contents);
+    let result: Option<i64> = find_high_score(recipe, Some(calorie_limit));
+    println!("Found score! {:?}", result);
     result
 }
 
 fn read_file(filename: String) -> String {
-    fs::read_to_string(filename).expect("Something went wrong reading the file")
+    fs::read_to_string(filename).expect("Something went wrong reading the file.")
 }
 
 fn parse_ingredients(contents: String) -> Recipe {
@@ -68,11 +82,11 @@ fn parse_ingredient(text: String) -> Ingredient {
     }
 }
 
-fn find_high_score(recipe: Recipe) -> i64 {
-    find_high_score_rec(&recipe, &mut vec![])
+fn find_high_score(recipe: Recipe, calorie_limit: Option<i64>) -> Option<i64> {
+    find_high_score_recursive(&recipe, &mut vec![], calorie_limit)
 }
 
-fn find_high_score_rec(recipe: &Recipe, multipliers: &mut Vec<i64>) -> i64 {
+fn find_high_score_recursive(recipe: &Recipe, multipliers: &mut Vec<i64>, calorie_limit: Option<i64>) -> Option<i64> {
     let new_vec = |vec: &Vec<i64>, elt: i64| {
         let mut a_new_vec = vec.clone();
         a_new_vec.push(elt);
@@ -81,16 +95,15 @@ fn find_high_score_rec(recipe: &Recipe, multipliers: &mut Vec<i64>) -> i64 {
     let count = multipliers.len();
     let recipe_length = recipe.len();
     if count >= recipe_length {
-        panic!("I miscounted the number of ingredients!!");
+        panic!("Proportion count != ingredient count!");
     } else if count == recipe_length - 1 {
         let total: i64 = multipliers.iter().sum();
         multipliers.push(100 - total);
-        recipe.score_with_multipliers(multipliers)
+        recipe.score_with_multipliers(multipliers, calorie_limit)
     } else {
         let total: i64 = multipliers.iter().sum();
-        (0..=(100 - total)).map(|i|
-            find_high_score_rec(recipe, &mut new_vec(multipliers, i))
-        ).max().expect("Out of bounds or empty?")
+        (0..=(100 - total)).filter_map(|i|
+            find_high_score_recursive(recipe, &mut new_vec(multipliers, i), calorie_limit)
+        ).max()
     }
 }
-
--- 2015/aoc_2015_15_cookie_recipe/tests/aoc_2015_15_cookie_recipe.rs
+++ 2015/aoc_2015_15_cookie_recipe/tests/aoc_2015_15_cookie_recipe.rs
@@ -5,13 +5,13 @@ use aoc_2015_15_cookie_recipe::*;
 #[test]
 fn test_make_cookies_input0() {
     let result = make_cookies(String::from("src/input0.txt"));
-    assert_eq!(result, 62842880);
+    assert_eq!(result, Some(62842880));
 }
 
 #[test]
 fn test_make_cookies_input1() {
     let result = make_cookies(String::from("src/input1.txt"));
-    assert_eq!(result, 13882464);
+    assert_eq!(result, Some(13882464));
 }
 
 #[test]
@@ -19,4 +19,36 @@ fn test_stdlib() {
     let v: Vec<i32> = vec![1, 2, 3].into_iter().map(|x| x + 1).rev().collect();
 
     assert_eq!(v, [4, 3, 2]);
+}
+
+#[test]
+fn test_cookies_with_calories_input0_500() {
+    let result = cookies_with_calories(String::from("src/input0.txt"), 500);
+    assert_eq!(result, Some(57600000));
+}
+
+#[test]
+fn test_cookies_with_calories_input1_500() {
+    let result = cookies_with_calories(String::from("src/input1.txt"), 500);
+    assert_eq!(result, Some(11171160));
+}
+
+#[test]
+fn test_cookies_with_negative_calories() {
+    let result = cookies_with_calories(String::from("src/input1.txt"), -1);
+    assert_eq!(result, None);
+}
+
+/////////////////////////////////
+
+fn add_to_inverse(int: i32) -> i32 {
+    let inverse = 1 / int;
+    int + inverse
+}
+
+#[test]
+// #[should_panic(expected = "Guess value must be less than or equal to 100")]
+#[should_panic(expected = "divide by zero")]
+fn test_asserting_should_panic() {
+    add_to_inverse(0);
 }
