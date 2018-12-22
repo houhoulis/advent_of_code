365464a85768550c9213841117ae43ea4fd1b97c
Chris Houhoulis <chris@chrishouhoulis.com>
2018-12-21 22:21:22 -0500

    AoC 2015 #16: Aunts Sue, part 2. (Crystal)

--- 2015/aoc_2015_16_aunts_sue.cr
+++ 2015/aoc_2015_16_aunts_sue.cr
@@ -1,6 +1,37 @@
-def detect_a_sue(sues, props)
+EXACT_PROPS = {
+  "children" => 3,
+  "samoyeds" => 2,
+  "akitas" => 0,
+  "vizslas" => 0,
+  "cars" => 2,
+  "perfumes" => 1
+}
+
+LOWER_LIMIT_PROPS = {
+  "cats" => 7,
+  "trees" => 3
+}
+
+UPPER_LIMIT_PROPS = {
+  "pomeranians" => 3,
+  "goldfish" => 5,
+}
+
+def matches_exact_props(sue)
+  (EXACT_PROPS.keys & sue.keys).none? { |k| sue[k].to_i != EXACT_PROPS[k] }
+end
+
+def exceeds_lower_limit_props(sue)
+  (LOWER_LIMIT_PROPS.keys & sue.keys).none? { |k| sue[k].to_i <= LOWER_LIMIT_PROPS[k] }
+end
+
+def below_upper_limit_props(sue)
+  (UPPER_LIMIT_PROPS.keys & sue.keys).none? { |k| sue[k].to_i >= UPPER_LIMIT_PROPS[k] }
+end
+
+def detect_a_sue(sues)
   matches = sues.select do |sue|
-    props.map { |k, v| v == sue[k] if sue.keys.includes?(k) }.compact.uniq == [true]
+    matches_exact_props(sue) && exceeds_lower_limit_props(sue) && below_upper_limit_props(sue)
   end
   raise "hello! #{matches.size}" unless matches.size == 1
 
@@ -9,19 +40,7 @@ end
 
 def solve
   sues = parse_input("aoc_2015_16_input.txt")
-  props = {
-    "children" => "3",
-    "cats" => "7",
-    "samoyeds" => "2",
-    "pomeranians" => "3",
-    "akitas" => "0",
-    "vizslas" => "0",
-    "goldfish" => "5",
-    "trees" => "3",
-    "cars" => "2",
-    "perfumes" => "1",
-  }
-  p detect_a_sue(sues, props)
+  p detect_a_sue(sues)
 end
 
 def parse_input(filename)
